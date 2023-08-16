91a39c7081cc8a66700258ad181c149577a6c69d
SophieMalka <s.malka@live.fr>
2023-08-16 10:54:53 +0200

    routage

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/home/home';
+import About from './pages/about/about';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
