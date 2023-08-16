c5b754d39810805e3b3d73eb8e917522cd28eacd
SophieMalka <s.malka@live.fr>
2023-08-16 15:19:14 +0200

    update

--- frontend/src/components/nav/nav.jsx
+++ frontend/src/components/nav/nav.jsx
@@ -6,7 +6,6 @@ function Nav() {
             <ul className='nav-list'>
                 <li className='nav-item'><a href='/'>Accueil</a></li>
                 <li className='nav-item'><a href='/about'>A propos</a></li>
-                <li className='nav-item'>Stack</li>
                 <li className='nav-item'>Portfolio</li>
                 <li className='nav-item'>Contact</li>
             </ul>
