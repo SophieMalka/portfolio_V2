f46ea4a200afe89c5c146550386f96417c299a32
SophieMalka <s.malka@live.fr>
2023-08-16 15:15:15 +0200

    add link

--- frontend/src/components/scroll/scroll.jsx
+++ frontend/src/components/scroll/scroll.jsx
@@ -1,14 +1,17 @@
 import './scroll.css';
+import { Link, animateScroll as scroll } from "react-scroll";
 
-function Scroll() {
+function Scroll( { link } ) {
     return(
-        <div class="arrow-container animated fadeInDown">
-            <div class="arrow-2"> 
-                <i class="fa fa-angle-down"></i>
-            </div>
-            <div class="arrow-1 animated hinge infinite zoomIn">
-            </div>
-        </div>
+        <Link to={link} smooth={true} duration={500}>
+            <button class="arrow-container animated fadeInDown">
+                <div class="arrow-2"> 
+                    <i class="fa fa-angle-down"></i>
+                </div>
+                <div class="arrow-1 animated hinge infinite zoomIn">
+                </div>
+            </button>
+        </Link>
     )
 };
 
